ab2621788619f9bc7c3359158747840db35baac6
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-04-30 17:01:03 +0600

    Error page designed

---
+++ src/app/auth/signin/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignInPage = () => {
+  return (
+    <div className="pt-25">
+      <p>SignInPage</p>
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ src/app/auth/signup/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUpPage = () => {
+  return (
+    <div className="pt-25">
+      <p>SignUpPage</p>
+    </div>
+  );
+};
+
+export default SignUpPage;
