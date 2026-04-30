0873b05971b1b6d59a0e9fc4bfacbe8a385eeb62
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-04-30 20:38:45 +0600

    All course section designed

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,9 +1,36 @@
+import MyContainer from "@/components/MyContainer/MyContainer";
+import CourseCard from "@/ui/CourseCard";
 import React from "react";
 
-const CoursesPage = () => {
+const getData = async () => {
+  const res = await fetch("https://skill-sphere-pi-inky.vercel.app/data.json", {
+    cache: "no-store",
+  });
+  return res.json();
+};
+
+const CoursesPage = async () => {
+  const courses = await getData();
   return (
     <div className="pt-25">
-      <p>Courses</p>
+      <MyContainer className="py-10">
+        <h2 className="text-2xl md:text-3xl font-bold text-center mb-10">
+          All Courses
+        </h2>
+        <div className="flex items-center justify-center">
+          <input
+            type="text"
+            className="input focus:border-none"
+            placeholder="Search"
+          />
+          <button className="btn">Search</button>
+        </div>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-10">
+          {courses.map((course) => (
+            <CourseCard key={course.id} course={course} />
+          ))}
+        </div>
+      </MyContainer>
     </div>
   );
 };
