15076105d0e212049e4ade0f2c59799e60e17b92
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-05-03 11:05:26 +0600

    Search function added

--- src/components/Courses/SearchBox.jsx
+++ src/components/Courses/SearchBox.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useState } from "react";
 
 const SearchBox = () => {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search");
+  const router = useRouter();
+
+  const [inputValue, setInputValue] = useState(search || "");
+
+  const handleSearch = () => {
+    const params = new URLSearchParams(searchParams.toString);
+    if (inputValue) {
+      params.set("search", inputValue);
+    } else {
+      params.delete("search");
+    }
+    router.push(`?${params.toString()}`);
+  };
+
+  console.log(search);
   return (
     <div className="flex items-center justify-center">
       <input
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
         type="text"
         className="input focus:border-none"
         placeholder="Search"
       />
-      <button className="btn">Search</button>
+      <button onClick={handleSearch} className="btn">
+        Search
+      </button>
     </div>
   );
 };
